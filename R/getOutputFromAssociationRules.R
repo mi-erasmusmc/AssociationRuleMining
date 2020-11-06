11018824b16a4f95c1b302a8084ebbb2856f49b9
Solon Ioannou <sioann88@gmail.com>
2020-11-06 17:21:31 +0100

    Fixing Association rules functions to i) read number of transactions IDs ii) filename issue. Wrapped functions running the java implementation of the algorithms in one call, into runAssociationRules().

--- R/getOutputFromAssociationRules.R
+++ R/getOutputFromAssociationRules.R
@@ -1,12 +1,13 @@
-getOutputFromAssociationRules <- function(inputFile) {
+getOutputFromAssociationRules <- function(inputFile, numberOfTransactionIDs) {
   inputfile = read.delim(inputFile, header = FALSE)
   x <- inputfile %>%
-    mutate(Support = stringr::str_replace_all(V1, ".*: ", ""),
-           Set = stringr::str_replace_all(V1, ".#.*", ""))
+    mutate(Count = stringr::str_replace_all(V1, ".*: ", ""),
+           Set = stringr::str_replace_all(V1, ".#.*", ""), 
+           Support = as.numeric(Count)/numberOfTransactionIDs)
   
   x$Set <- stringr::str_replace_all(x$Set, " ", ", ") 
   x$Set <- stringr::str_replace_all(x$Set, "_", " ") 
   x$Set <- stringr::str_replace_all(x$Set, ", $", "")
-  x <- dplyr::select(x, c(Set, Support))
+  x <- dplyr::select(x, c(Set, Count, Support))
   return(x)
 }
