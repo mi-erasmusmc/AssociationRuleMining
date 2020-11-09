7738afc67ecf7f1019c27caf140fbf1d73e58eb5
Solon Ioannou <sioann88@gmail.com>
2020-11-09 17:44:42 +0100

    Adding package depencencies within function calls.

--- R/getIdDataFrame.R
+++ R/getIdDataFrame.R
@@ -1,14 +1,14 @@
 getIdDataFrame <- function(inputFile){
   inputfile = read.delim(inputFile, header = FALSE)
   y <- inputfile %>%
-    mutate(id = stringr::str_replace_all(V1, ".*SID: ", ""), 
-           Seqs = stringr::str_replace_all(V1, ".#.*", "")) %>%
-    select(Seqs, id) %>%
-    separate_rows(id, sep = " ") %>%
-    mutate(val=1) %>% 
-    pivot_wider(names_from = Seqs, values_from = val) %>% 
-    mutate(across(-id, ~replace_na(.x, 0)))%>%
-    arrange(as.numeric(id))
+    dplyr::mutate(id = stringr::str_replace_all(V1, ".*SID: ", ""), 
+                  Seqs = stringr::str_replace_all(V1, ".#.*", "")) %>%
+    dplyr::select(Seqs, id) %>%
+    tidyr::separate_rows(id, sep = " ") %>%
+    dplyr::mutate(val=1) %>% 
+    tidyr::pivot_wider(names_from = Seqs, values_from = val) %>% 
+    dplyr::mutate(across(-id, ~replace_na(.x, 0)))%>%
+    dplyr::arrange(as.numeric(id))
   
   y$id <- as.numeric(y$id) + 1  # Adding one to match Java's and R's enum type
 
