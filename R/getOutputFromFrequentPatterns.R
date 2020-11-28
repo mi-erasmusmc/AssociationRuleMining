65b37c882862cc1a5b236d08f556f3c708028cf1
Solon Ioannou <sioann88@gmail.com>
2020-11-28 17:22:39 +0100

    Adding algorithms for mining - closed patterns (Clasp, CM-Clasp) - maximal patterns (VMSP) - generator sequential patterns (VGEN) - pattern rules (RuleGrowth, ERMiner) Updating relevant functions - getFrequentPatterns - getOutputFromFrequentPatterns - runFrequentPatterns

--- R/getOutputFromFrequentPatterns.R
+++ R/getOutputFromFrequentPatterns.R
@@ -1,26 +1,44 @@
 getOutputFromFrequentPatterns <- function(inputFile, numberOfSequenceIDs, showID) {
   inputfile = read.delim(inputFile, header = FALSE)
   
-  if (showID == FALSE){
+  # Condition when agorithm extracted Rules
+  if (any(stringi::stri_detect_fixed(inputfile$V1, "#CONF", max_count = 1) == TRUE) == TRUE) {
+    confidenceIncluded <- TRUE
+  } else {
+    confidenceIncluded <- FALSE
+  } 
+  
+  if (showID == FALSE && confidenceIncluded == FALSE){
     x <- inputfile %>%
       dplyr::mutate(Count = stringr::str_replace_all(V1, ".*: ", ""),
                     Sequence = stringr::str_replace_all(V1, ".#.*", ""), 
-                    Support = as.numeric(Count)/numberOfSequenceIDs)
+                    Support = as.numeric(Count)/numberOfSequenceIDs) %>%
+      dplyr::select(Sequence, Count, Support)
     
     x$Sequence <- stringr::str_replace_all(x$Sequence, " -1", " =>") 
     x$Sequence <- stringr::str_replace_all(x$Sequence, "_", " ") 
     x$Sequence <- stringr::str_replace_all(x$Sequence, "=>$", "")
-  } else {
+    
+  } else if (showID == TRUE && confidenceIncluded == FALSE) {
     x <- inputfile %>%
       dplyr::mutate(Count = stringr::str_match(inputfile$V1, "#SUP: \\s*(.*?)\\s*#SID:")[,2],
                     Sequence = stringr::str_replace_all(V1, ".#.*", ""), 
-                    Support = as.numeric(Count)/numberOfSequenceIDs)
+                    Support = as.numeric(Count)/numberOfSequenceIDs) %>%
+      dplyr::select(Sequence, Count, Support)
     
     x$Sequence <- stringr::str_replace_all(x$Sequence, " -1", " =>") 
     x$Sequence <- stringr::str_replace_all(x$Sequence, "_", " ") 
     x$Sequence <- stringr::str_replace_all(x$Sequence, "=>$", "")
+    
+  } else if (confidenceIncluded == TRUE) {
+    x <- inputfile %>%
+      dplyr::mutate(Count = stringr::str_match(V1, "#SUP: \\s*(.*?)\\s*#CONF:")[,2],
+                    Sequence = stringr::str_replace_all(V1, ".#.*", ""), 
+                    Support = as.numeric(Count)/numberOfSequenceIDs,
+                    Confidence = round(as.numeric(stringr::str_replace_all(V1, ".*CONF: ", "")), digits = 7)) %>%
+      dplyr::select(Sequence, Count, Support, Confidence)
   }
   
-  x <- dplyr::select(x, c(Sequence, Count, Support))
+  #x <- dplyr::select(x, c(Sequence, Count, everything()))
   return(x)
 }
--- R/runFrequentPatterns.R
+++ R/runFrequentPatterns.R
@@ -1,11 +1,11 @@
-runFrequentPatterns <- function(algorithm, inputFile, outputFile, minsup, minLength = 1 , maxLength = Inf , maxGap = Inf, showID = FALSE) {
+runFrequentPatterns <- function(algorithm, inputFile, outputFile, minsup, minconf, minLength = 1 , maxLength = Inf , maxGap = Inf, maxAntecedentLength = Inf, maxConsequentLength = 1, showID = FALSE) {
   datafile <- read.delim(paste(inputFile), header = FALSE, blank.lines.skip = TRUE, comment.char = "@")
   noTIDs <- nrow(datafile)
   
   cat(paste("Analysing", noTIDs, "sequence IDs...", sep = " "))
   
   cat("Running frequent pattern algorithm...")
-  getFrequentPatterns(algorithm, inputFile = inputFile, outputFile = outputFile, minsup = minsup, showID = showID)
+  getFrequentPatterns(algorithm, inputFile = inputFile, outputFile = outputFile, minsup = minsup, minconf = minconf, minLength, maxLength, maxGap, maxAntecedentLength, maxConsequentLength, showID = showID)
   
   cat("Preparing output...")
   getOutputFromFrequentPatterns(inputFile = outputFile, numberOfSequenceIDs = noTIDs, showID)
