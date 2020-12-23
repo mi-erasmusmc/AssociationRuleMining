6479d400c7e1f28af060865524363998b8f12ed0
Solon Ioannou <sioann88@gmail.com>
2020-12-23 17:10:04 +0100

    - Fixing bug to read true rowId in getNamesFromCovariateId()

--- R/getNamesFromCovariateId.R
+++ R/getNamesFromCovariateId.R
@@ -12,14 +12,18 @@ getNamesFromCovariateId <- function(data, covariateDataObject, fileToSave){
   write.table(names.df$SPMFnameID, paste(fileToSave), quote = FALSE, row.names = FALSE, col.names = FALSE, append = TRUE)
   data$covariateId <- as.character(data$covariateId)
   data2 <- tidyr::separate_rows(data, covariateId)
-  df_input <- dplyr::inner_join(data2, names.df, by = "covariateId") %>%
-    dplyr::group_by(rowId) %>%
-    dplyr::mutate(Java_Row_Id = dplyr::row_number()-1) %>%
-    dplyr::ungroup()
+  df_input <- dplyr::inner_join(data2, names.df, by = "covariateId")
+  
+  SPMFrowId <- data.frame(trueId = unique(data2$rowId)) %>%
+    dplyr::arrange(trueId) %>%
+    dplyr::mutate(SPMFrowId = dplyr::row_number() - 1)
+  
+  df_input <- dplyr::inner_join(df_input, SPMFrowId, by = c("rowId" = "trueId")) %>%
+    dplyr::arrange(rowId)
   # Filtering useful variables
   #  df_input2 <- dplyr::select(df_input, c(rowId, eventId, SIZE, covariateLabel))
   #  return(df_input2)
-  row_id <- unique(df_input$rowId)
+  #row_id <- unique(df_input$rowId)
   #result <- list(df_input, row_id)
   #return(result)
   return(df_input)
