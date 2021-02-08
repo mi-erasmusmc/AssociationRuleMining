0242a1edaf2d3eb5acadcd3712887729882bacbc
Solon Ioannou <sioann88@gmail.com>
2021-02-08 18:31:05 +0100

    Adding execution times summary.

--- R/getFrequentPatternsCovariateData.R
+++ R/getFrequentPatternsCovariateData.R
@@ -19,7 +19,7 @@ getFrequentPatternsCovariateData <- function(connection,
     stop("Argument 'ShowId' is FALSE. No reason to run this analysis.")
   
   # Extracting covariates 
-  
+  t1start <- Sys.time()
   temporalData <- FeatureExtraction::getDbCovariateData(connection = connection, 
                                                         cdmDatabaseSchema = cdmDatabaseSchema, 
                                                         cohortDatabaseSchema = covariateSettings$cohortDatabaseSchema, 
@@ -29,23 +29,50 @@ getFrequentPatternsCovariateData <- function(connection,
                                                         covariateSettings = covariateSettings$temporalCovariateSettings, 
                                                         cohortTableIsTemp = covariateSettings$isCohortTableTemp) 
   
+  t1duration <- Sys.time() - t1start
+  
+  message(paste0("Extracting covariates took", round(t1duration, 2), "minutes."))
+  
+  # Saving covariate data object 
+  ## this should be saveed in a formal location folder ie. "analysis folder" as it is a costly operation
   #FeatureExtraction::saveCovariateData(temporalData, file = "temporalCovariateData")
   
   # Preparing dataset
   writeLines("Preparing input for running frequent pattern analysis...")
+  
+  t2start <- Sys.time()
+  
   input <- getInputFileForFrequentPatterns(covariateDataObject = temporalData, fileToSave = covariateSettings$inputFile)
+  
+  t2duration <- Sys.time() - t2start
+  
+  message(paste0("Preparing input dataset took", round(t2duration, 2), "minutes."))
   
   # Getting frequent patterns
   writeLines("Running frequent pattern analysis...")
+  
+  t3start <- Sys.time()
+  
   frequentPatterns <- runFrequentPatterns(algorithm = covariateSettings$algorithm, 
                                           inputFile = covariateSettings$inputFile, 
                                           outputFile = covariateSettings$outputFile, 
                                           minsup = covariateSettings$minsup, 
                                           showID = covariateSettings$showID)
   
+  t3duration <- Sys.time() - t3start
+  
+  message(paste0("Extracting frequent patterns took", round(t3duration, 2), "minutes."))
+  
   # Converting to FeatureExtraction object
   writeLines("Creating covariates out the extracted patterns...")
+  
+  t4start <- Sys.time()
+  
   fpData <- toCovariateData(inputFile = covariateSettings$outputFile, objectWithIds = input)
+  
+  t4duration <- Sys.time() - t4start 
+  
+  message(paste0("Creating covariate data object took", round(t4duration, 2), "minutes."))
   
   assign("frequentPatternsData", frequentPatterns, envir = .GlobalEnv)
   return(fpData)
