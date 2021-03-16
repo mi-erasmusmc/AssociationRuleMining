7a96a19866ab8e57895ad153344e64e94901ebb7
Solon Ioannou <sioann88@gmail.com>
2021-03-16 11:51:02 +0100

    Adding five new algorithms - CM-SPADE (FPM) - CM-SPAM (FPM) - BIDE+ (closed set) - TopSeqRules (Top K sequential rules) - TNS (Top K non redundant seq. rules)

--- R/getFrequentPatterns.R
+++ R/getFrequentPatterns.R
@@ -1,4 +1,4 @@
-getFrequentPatterns <- function(algorithm, inputFile, outputFile, minsup, minconf, minLength = 1, maxLength = Inf, maxGap = Inf, maxAntecedentLength = Inf, maxConsequentLength = 1, showID = FALSE) {
+getFrequentPatterns <- function(algorithm, inputFile, outputFile, minsup, minconf, minLength = 1, maxLength = Inf, maxGap = Inf, maxAntecedentLength = Inf, maxConsequentLength = 1, top_k = 1, delta = 0, showID = FALSE) {
   
   spmf.dir <- rJava::.jclassPath()[stringr::str_ends(rJava::.jclassPath(), "spmf.jar")]
   frequentSequencesAlgorithms <- c("SPAM", "SPADE", "prefixSpan", "Clasp", "CM-Clasp", "VMSP", "VGEN", "RuleGrowth", "ERMiner")
@@ -8,12 +8,18 @@ getFrequentPatterns <- function(algorithm, inputFile, outputFile, minsup, mincon
   #maxGapvalue = jdx::convertToJava(maxGap, scalars.as.objects = TRUE)
   if (maxLength == Inf) {
   maxLengthValue = as.integer(1000000000)
+  } else {
+    maxLengthValue = maxLength
   }
   if (maxGap == Inf){ 
     maxGapValue = as.integer(1000000000)
+  } else{
+    maxGapValue = maxGap
   }
   if (maxAntecedentLength == Inf){
   maxAntecedentLength = as.integer(1000000000)
+  } else {
+    maxAntecedentLength
   }
   
   maxConsequentLength = maxConsequentLength
@@ -23,27 +29,42 @@ getFrequentPatterns <- function(algorithm, inputFile, outputFile, minsup, mincon
   }
   
   #Below I replaced the Inf values for maxLength and maxGap with 1000 since Inf is not a Java object for SPAM and prefixSpan
+    # Frequent Pattern Mining
   if (algorithm == "SPAM" ) {
     executable <- paste("java -jar", spmf.dir, "run", algorithm, inputFile, outputFile, minsup, minLength, maxLengthValue, maxGapValue, outputID, sep = " ")
   } else if (algorithm == "SPADE") {
-      executable <- paste("java -jar", spmf.dir, "run", algorithm, inputFile, outputFile, minsup, outputID, sep = " ")
-  } else if (algorithm == "prefixSpan"){
-      executable <- paste("java -jar", spmf.dir, "run", "PrefixSpan", inputFile, outputFile, minsup, maxLengthValue, outputID, sep = " ")
+    executable <- paste("java -jar", spmf.dir, "run", algorithm, inputFile, outputFile, minsup, outputID, sep = " ")
+  } else if (algorithm == "prefixSpan") {
+    executable <- paste("java -jar", spmf.dir, "run", "PrefixSpan", inputFile, outputFile, minsup, maxLengthValue, outputID, sep = " ")
+  } else if (algorithm == "CM-SPADE") {
+    executable <- paste("java -jar", spmf.dir, "run", algorithm, inputFile, outputFile, minsup, outputID, sep = " ")
+  } else if (algorithm == "CM-SPAM") {
+    executable <- paste("java -jar", spmf.dir, "run", algorithm, inputFile, outputFile, minsup, minLength, maxLengthValue, maxGapValue, outputID, sep = " ")
   } else if (algorithm == "Clasp") {
-      executable <- paste("java -jar", spmf.dir, "run", "ClaSP", inputFile, outputFile, minsup, outputID, sep = " ")
+    # Closed patterns
+     executable <- paste("java -jar", spmf.dir, "run", "ClaSP", inputFile, outputFile, minsup, outputID, sep = " ")
   } else if (algorithm == "CM-Clasp"){
      executable <- paste("java -jar", spmf.dir, "run", "CM-ClaSP", inputFile, outputFile, minsup, outputID, sep = " ")
+  } else if (algorithm == "Bide+") {
+    executable <- paste("java -jar", spmf.dir, "run", "BIDE+", inputFile, outputFile, minsup, outputID, sep = " ")
   } else if (algorithm == "VMSP") {
+    # Maximal patters
     executable <- paste("java -jar", spmf.dir, "run", "VMSP", inputFile, outputFile, minsup, maxLengthValue, maxGapValue, outputID, sep = " ")
   } else if (algorithm == "VGEN") {
+    # Sequential generators
     executable <- paste("java -jar", spmf.dir, "run", algorithm, inputFile, outputFile, minsup, maxLengthValue, maxGapValue, outputID, sep = " ")
   } else if (algorithm == "RuleGrowth") {
+    # Sequential Rules
     executable <- paste("java -jar", spmf.dir, "run", algorithm, inputFile, outputFile, minsup, minconf, maxAntecedentLength, maxConsequentLength, outputID, sep = " ")
   } else if (algorithm == "ERMiner") {
     executable <- paste("java -jar", spmf.dir, "run", algorithm, inputFile, outputFile, minsup, minconf, maxAntecedentLength, maxConsequentLength, outputID, sep = " ")
+  } else if (algorithm == "TNS") {
+    # Top K sequential rules
+    executable <- paste("java -jar", spmf.dir, "run", algorithm, inputFile, outputFile, top_k, minconf, delta, sep = " ")
+  } else if (algorithm == "TopSeqRules") {
+    executable <- paste("java -jar", spmf.dir, "run", algorithm, inputFile, outputFile, top_k, minconf, sep = " ") 
   }
     
-  
 
   cat(paste("The command line that has been running is:", print(executable)))
   system(executable)
