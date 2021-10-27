4e19728c1d0f4288e53e59455b7392eb44d484d6
Solon Ioannou <sioann88@gmail.com>
2021-10-27 16:56:27 +0200

    -Added comments to runFrequentPatterns() to export it. - .tex files removed from examples - NAMESPACE update - man update

--- R/runFrequentPatterns.R
+++ R/runFrequentPatterns.R
@@ -1,3 +1,16 @@
+#' Run Frequent Patterns algorithms
+#'
+#' \code{runFrequentPatterns} returns the extracted frequent patterns.
+#'
+#' The function takes a .txt file as input and returns the results in a .txt file as well as a tibble. 
+#' 
+#' @param algorithm One between "SPAM", "SPADE", "prefixSpan", "CM-SPADE", "CM-SPAM", "Clasp", "CM-Clasp", "BIDE+", "VMSP", "VGEN", "RuleGrowth", "ERMiner", "TNS", "TopSeqRules". 
+#' @param inputFile Name of the .txt file to be processed.
+#' @param outputFile Name of .txt file to extract results.
+#' @param minsup Value of minimum support between 0-1.
+#' @param minconf Value of minimum confidence. Only some algorithms are making use of this (TO be specified)
+#' @export
+
 runFrequentPatterns <- function(algorithm, inputFile, outputFile, minsup, minconf, minLength = 1 , maxLength = Inf , maxGap = Inf, maxAntecedentLength = Inf, maxConsequentLength = 1, showID = FALSE) {
   datafile <- read.delim(paste(inputFile), header = FALSE, blank.lines.skip = TRUE, comment.char = "@")
   noTIDs <- nrow(datafile)
